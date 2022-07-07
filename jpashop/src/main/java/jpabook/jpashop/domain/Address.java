642a7e4f2f2e467591dc52f6291ee28521b2477b
junsub.hyun <guswnstjq10@gmail.com>
2022-07-07 15:19:00 +0900

    엔티티 클래스 생성

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,4 +12,13 @@ public class Address {
     private String street;
     private String zipcode;
 
+    protected Address() {
+        // JPA 스펙상 생성
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
