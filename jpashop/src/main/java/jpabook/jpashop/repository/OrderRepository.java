061fd689aa69986f5b15b3864d57a99ffbf7b74a
JunSubHyun <guswnstjq10@gmail.com>
2022-08-09 12:34:37 +0900

    API 조회 성능 최적화

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -104,6 +104,7 @@ public class OrderRepository {
                         " join fetch o.delivery d",Order.class
         ).getResultList();
     }
+    
 
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery(
@@ -123,6 +124,16 @@ public class OrderRepository {
                 " join fetch o.delivery d"+
                 " join fetch o.orderItems oi"+
                 " join fetch oi.item i" , Order.class).getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o"+
+                        " join fetch o.member m"+
+                        " join fetch o.delivery d",Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
 }
 
